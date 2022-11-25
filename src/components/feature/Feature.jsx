3f5b7d5527c79363b2f88df1569ebcfe0a713003
Sen Cao <caosen.cn@gmail.com>
2022-11-25 21:03:25 +0200

    finihsed the about section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,7 +1,19 @@
 import './feature.css'
 
-export function Feature() {
+export function Feature({title, text, pic}) {
   return(
-    <div>Feature</div>
+    <div className="wf__featues-container__feature">
+      <div className="wf__features-container__feature-pic">
+        <img src={pic} alt="illustration" />
+        <div/>
+      </div>
+      <div className="wf__features-container__feature-title">
+        <h1>{title}</h1>
+        <div/>
+      </div>
+      <div className="wf__features-container__feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
   )
 }
--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,23 +1,25 @@
 import './about.css'
 import { Feature } from '../../components'
+import vote from '../../assets/vote.png'
+import decentralized from '../../assets/decentralized.png'
+import secure from '../../assets/secure.png'
+import happy from '../../assets/happy.png'
 
 export function About() {
   return(
     <div className="section__margin wf__about" id="about">
-      <div className="wf__about-feature">
-        <Feature />
-      </div>
       <div className="wf__about-heading">
         <h1 className="gradient__text">
-          When stars shine, the void is lit
+          An exploration and a future defination of Web 3.0
         </h1>
-        <p>Check out the outlook</p>
       </div>
       <div className="wf__about-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title="What is WishFund?" text="It is a   platform where you either choose an existing fund to   participate or start a new fund project, a fund   participant will follow full democratic principles to  determine the use of the fund." pic={happy}/>
+        <Feature title="Decentralization" text="At the dawn of Web3, WishFund is not afraid of exploring its possibilities. WishFund is giving the full management to fund participants as opposed to normal funding service where participants put full confidence in a central entity." pic={decentralized}/>
+        <Feature title="Full Democracy" text="The platform is open to all, all shall be entitled with full democratic rights, each fund itself is a mini society where full democracy is practised." pic={vote}/>
+        <Feature title="Secure Finance" text="WishFund will be powered by Ethernet, the Smart Contract principle will guide all transactions within the funds." pic={secure}/>
       </div>
+
     </div>
   )
 }
