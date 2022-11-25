2b6ccd35f3c0c4092512d29eab328285eec57164
Sen Cao <caosen.cn@gmail.com>
2022-11-25 23:48:34 +0200

    the footer is finished

--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -1,7 +1,35 @@
 import './footer.css'
+import logo from '../../assets/logo.png'
 
 export function Footer() {
   return(
-    <div>Footer</div>
+    <div className="wf__footer">
+      <div className="wf__footer-links section__padding ">
+        <div className="wf__footer-links_logo">
+          <img src={logo} alt="wishfund logo" />
+          <p>All Rights Reserved</p>
+        </div>
+        <div className="wf__footer-links_div">
+          <h4>Contact</h4>
+          <p>Email</p>
+          <p>FaceBook</p>
+          <p>Other Social Media</p>
+        </div>
+        <div className="wf__footer-links_div">
+          <h4>Regulations</h4>
+          <p>Terms & Conditions</p>
+          <p>Pravacy Policy</p>
+        </div>
+        <div className="wf__footer-links_div">
+          <h4>It starts here</h4>
+          <p>Tel Aviv, Israel</p>
+          <p>Project initialized by Sen Cao</p>
+          <p>caosen.cn@gmail.com</p>
+        </div>
+      </div>
+      <div className="wf__footer-copyright">
+        <p>@2023 WishFund. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
