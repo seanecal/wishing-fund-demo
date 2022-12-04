5b2c435785b0deb193b0f41df3bb8e0c0cea4734
Sen Cao <caosen.cn@gmail.com>
2022-12-04 19:22:27 +0200

    deloy bug fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-export {Article} from './article/Article'
-export {Brand} from './brand/Brand'
 export {Cta} from './cta/Cta'
 export {Feature} from './feature/Feature'
 export {Navbar} from './navbar/Navbar'
